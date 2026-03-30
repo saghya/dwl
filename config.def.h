17df22721c668b0f95562d3153c86d995789155c
saghya <saghyattila77@gmail.com>
2026-03-30 21:17:11 +0200

    faster terminal

--- config.def.h
+++ config.def.h
@@ -136,7 +136,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-#define TERMINAL "foot"
+#define TERMINAL "footclient"
 
 /* commands */
 static const char *termcmd[]      = { TERMINAL, NULL };
